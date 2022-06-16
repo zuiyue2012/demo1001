47d5f17a7b2a63b337b3e4aa9f1725a7a84def95
zuiyue2012 <331685622@qq.com>
2022-06-16 23:28:54 +0800

    add user

--- src/main/java/com/example/demo1001/Demo1001Application.java
+++ src/main/java/com/example/demo1001/Demo1001Application.java
@@ -1,10 +1,10 @@
 package com.example.demo1001;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 
 @SpringBootApplication()
 @MapperScan(value = "com.example.demo1001.mapper")
